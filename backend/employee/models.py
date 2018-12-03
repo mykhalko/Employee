2b7adbde31ae69ea4308ea225bb359e883d7ad70
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-12-03 23:33:30 +0200

    Add model Employee
    direct fields:
     - fullname (str)
     - position (str)
     - salary (decimal number)
     - employment_date (DateTime)
     - superior (ForeignKey to self)
    
    indirect fields:
     - subordinates (superior_set)
    
    methods:
     - delete (with option to subdue subordinates to another employee)
     - delete_branch (delete full brnch of employees)
     - is_subdued_to (check if employee subdued to another one)
     - __str__ representation

---
+++ backend/employee/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Employee(models.Model):
+
+    fullname = models.CharField(max_length=255, null=False, blank=False)
+    position = models.CharField(max_length=127, null=False, blank=False)
+    employment_date = models.DateField(null=False, blank=False)
+    salary = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    superior = models.ForeignKey('employee.Employee', related_name='subordinates',
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.pk) + '. ' + str(self.fullname) + ' ' + str(self.position)
+
+    def delete(self, *args, subdue_to=None, **kwargs):
+        if subdue_to is not None:
+            if not isinstance(subdue_to, Employee):
+                raise TypeError('subdue_to argument must be employee.Employee instance')
+            if subdue_to.is_subdued_to(self):
+                raise ValueError(str(subdue_to) + ' is subdued to Employee for delete')
+            for subordinate in self.subordinates.all():
+                subordinate.superior = subdue_to
+                subordinate.save()
+        self.delete(*args, **kwargs)
+
+    def delete_branch(self):
+        for subordinate in self.subordinates.all():
+            subordinate.delete_branch()
+        self.delete()
+
+    def is_subdued_to(self, employee):
+        superior = self.superior
+        while superior:
+            if superior == employee:
+                return True
+            superior = superior.superior
+        return False
+
