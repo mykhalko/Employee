9afbd9916e7bc0df295a60875dd8d2bbf4fd99d1
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-12-09 13:29:50 +0200

    Fix employee serializer superior validation

--- backend/employee/serializers.py
+++ backend/employee/serializers.py
@@ -20,26 +20,23 @@ class EmployeeSerializer(serializers.ModelSerializer):
         return instance
 
     def update(self, instance, validated_data):
-        model = self.Meta.model
-        superior_id = validated_data.get('superior')
-        if superior_id:
-            superior = model.objects.get(id=superior_id)
-            validated_data['superior'] = superior
+        # model = self.Meta.model
+        # superior = validated_data.get('superior')
+        # if superior_id:
+        #     superior = validated_data
+        #     validated_data['superior'] = superior
         for key in validated_data:
             setattr(instance, key, validated_data[key])
         instance.save()
         return instance
 
-    def validate_superior(self, superior_id):
+    def validate_superior(self, superior):
         model = self.Meta.model
         if self.instance:
-            superior_queryset = model.objects.filter(id=superior_id)
-            if superior_queryset.exists():
-                superior = superior_queryset.first()
-                if superior.is_subdued_to(self.instance):
-                    raise ValidationError('Incorrect superior_id; superior is \
-                    subordinate of instance to update')
-        return superior_id
+            if superior.is_subdued_to(self.instance):
+                raise ValidationError('Incorrect superior id; superior is \
+                subordinate of instance to update')
+        return superior
 
     def validate_image(self, image):
         if image.size > 1048576:
